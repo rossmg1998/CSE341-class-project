b1146f3ad278c1eacf5ee64d6aa064a2d5b68d92
Ross Gardner <gar17014@byui.edu>
2024-05-24 11:08:09 -0600

    Added swagger

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+router.use('/', require('./swagger'));
 
 router.use('/brandon-mull-books.js', require('./brandon-mull-books.js'));
 router.use('/harry-potter-movies.js', require('./harry-potter-movies.js'));
---
+++ swagger.js
@@ -0,0 +1,23 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'My API',
+    description: 'API',
+  },
+  // change to render link
+  host: 'cse341-class-project-wuch.onrender.com/',
+  // change to https
+  schemes: ['https'],
+};
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['./routes/index.js'];
+
+// generate swagger.json
+swaggerAutogen(outputFile, endpointsFiles, doc);
+
+// Run server after it gets generated
+// swaggerAutogen(outputFile, endpointsFiles, doc).then(async () => {
+//   await import('./index.js');
+// });
